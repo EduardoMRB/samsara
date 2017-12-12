9a9789e9212c17bba1230367e7e8c88c35a9bd19
EC2 Default User <ec2-user@ip-172-31-31-158.ec2.internal>
2017-12-12 13:47:16 +0000

    build changes

--- ingestion-api/docker/configure-and-start.sh
+++ ingestion-api/docker/configure-and-start.sh
@@ -5,4 +5,11 @@
 # KAFKA_PORT_9092_TCP_ADDR: REQUIRED: (one or more)
 # RIEMANN_PORT_5555_TCP_ADDR: OPTIONAL
 
+export OUTPUT_TOPIC=${OUTPUT_TOPIC}
+export BROKER_LIST=${BROKER_LIST}
+export TRACKING_ENABLED=${TRACKING_ENABLED:-false}
+export RIEMANN=${RIEMANN}
+
+synapse /opt/ingestion-api/config/config.edn.tmpl
+
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
